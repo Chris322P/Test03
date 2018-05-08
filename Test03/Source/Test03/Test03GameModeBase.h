f1173b73f562b6ad1ce00b07929208b949e89967
DESKTOP-IQAIMOV\chris <Chris322P@googlemail.com>
2018-05-08 12:46:36 +0200

    Inital Commit

---
+++ Test03/Source/Test03/Test03GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Test03GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST03_API ATest03GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
