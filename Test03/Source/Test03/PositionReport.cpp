9bf11a3ec86ae83ee9c246b275981d57b2d1ff55
DESKTOP-IQAIMOV\chris <Chris322P@googlemail.com>
2018-05-12 15:09:49 +0200

    InfInt added + Baisc Map test

--- Test03/Source/Test03/PositionReport.cpp
+++ Test03/Source/Test03/PositionReport.cpp
@@ -2,6 +2,13 @@
 
 #include "PositionReport.h"
 #include "GameFramework/Actor.h" //This is the change in PositionReport.cpp
+#include "InfInt.h"
+
+InfInt myint1 = "15432154865413186646848435184100510168404641560358";
+InfInt myint2 = 156341300544608LL;
+InfInt tstInt1 = "10000000000000000";
+InfInt tstInt2 = "";
+
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -22,18 +29,41 @@ void UPositionReport::BeginPlay()
 
 	FString ObjectName = GetOwner()->GetName();
 	FString ObjectPos = GetOwner()->GetActorTransform().GetLocation().ToString();
+	AActor *actor = GetOwner();
+	FRotator rotation = FRotator(50.0f, 50.0f, 0.0f);
+	actor->SetActorRotation(rotation);
 
 	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
+
+
+	myint1 *= --myint2 - 3;
+	std::cout << myint1 << std::endl;
+
+	std::string test = myint1.toString();
+
+
+	FString test2 = tstInt1.toString().c_str();
+
+	InfInt testInt = tstInt1 + 1;
+
+	FString test3 = testInt.toString().c_str();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s _____ %s"), *test2, *test3);
 	// ...
 	
 }
 
+InfInt infTest = 1;
 
 // Called every frame
 void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	//infTest = infTest * 2;
 
+	//FString test3 = infTest.toString().c_str();
+
+	//UE_LOG(LogTemp, Warning, TEXT("Zahl: %s"), *test3);
 	// ...
 }
 
